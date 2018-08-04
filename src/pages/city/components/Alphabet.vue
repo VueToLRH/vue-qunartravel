e1acc9782364204c5202a58053527d4ec5eea8d2
lingronghai <837233792@qq.com>
2018-08-04 10:03:13 +0800

    perfomance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,17 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      @click="handleLetterClick"
+    >
+      {{item}}
+    </li>
   </ul>
 </template>
 
@@ -9,6 +20,53 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0, // 用于提高手指移动的性能，因为如果定义在函数中，则每次移动都需要计算，所以进行提出
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop // 当页面进行跟鞋的时候（即：页面请求ajax进行重新渲染的时候，数字列表其实已经加载完成，即可以进行计算 A 字母的距离）
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      // 函数节流
+      // 设置定时器，当手指移动的时候，延迟16毫秒执行，如果在16毫米之内再次进行移动则会将之前的时间进行清除。可以大大减少 handleTouchMove 的执行频率，提高性能
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          // const startY = this.$refs['A'][0].offsetTop
+          const touchY = e.touches[0].clientY - 79 // 79为城市选择和说搜索框的高度之和
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
